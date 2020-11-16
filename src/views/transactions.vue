0460ae6d8ac6912250b930511d62059aa60c1701
随风 <1229674380@qq.com>
2020-11-16 22:23:06 +0800

    2020-11-16 22:23

--- src/views/transactions.vue
+++ src/views/transactions.vue
@@ -5,40 +5,34 @@
             
             <div class="container">
                 <div class="page-tilte pb-3 pt-3">区块</div>
-                <div  class=" tab-css tab-box">
-                    <el-table class="tds-table tds-trans-table"  :cell-class-name="cellClass" :row-class-name="tableRowClassName"
-                        :data="blockList"
-                        style="width: 100%">
-                        <el-table-column prop="height" label="事务hash"  >
-                            <template slot-scope="scope">
-                                <div><a class="line1">{{scope.row.hash}}</a></div>      
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="nonce" label="区块高度"  class="tab-height" >
-                            <template slot-scope="scope">
-                                <div><a >{{scope.row.nonce}}</a></div>      
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="created_at" label="出块时间"  >
-                           <template slot-scope="scope">
-                              <div><span>{{scope.row.created_at |　timefilters}}</span></div>                           
-                           </template>
-                        </el-table-column>
-                        <el-table-column prop="from" label="from" >
-                            <template slot-scope="scope">
-                                <div><a class="line1">{{scope.row.from}}</a></div>      
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="to" label="to" >
-                            <template slot-scope="scope">
-                                <div><a class="line1">{{scope.row.from}}</a></div>      
-                            </template>
-                        </el-table-column>
-                   
-                      
-                        <el-table-column prop="amount" label="amount" ></el-table-column>
-                        <el-table-column prop="fee" label="手续费"  ></el-table-column>
-                    </el-table>
+                <div  class=" tab-css tab-box ">
+                	<div class="com-table-box">
+                	  <table class="com-table">
+                	  	 <tr>
+                	  	 	 <th>事务hash</th>
+                	  	 	 <th class="tab-th">区块高度
+                	  	 	 	  <span class="caret-wrapper" :class="{'ascending':sortType==1,'descending':sortType==2}">
+                	  	 	 	  	<i class="sort-caret ascending" @click="sort(1)"></i>
+                	  	 	 	  	<i class="sort-caret descending" @click="sort(2)"></i>
+                	  	 	 	  </span>
+                	  	 	 </th>
+                	  	 	 <th>出块时间</th>
+                	  	 	 <th>from</th>
+                	  	 	 <th>to</th>
+                	  	 	 <th>amount</th>
+                	  	 	 <th>手续费</th>
+                	  	 </tr>
+                	  	 <tr v-for="(item,index) in blockList">
+                	  	 	<td><div class="d-hash "><a class="line1">{{item.hash}}</a></div></td>
+                	  	 	<td><div><span class="text-pri-default"><a>{{item.nonce}}</a></span></div></td>
+                	  	 	<td><div class=""><span class="text-pri-default">{{item.created_at}}</span></div></td>
+                	  	 	<td><div class="d-hash"><a class="line1">{{item.from}}</a></div></td>
+                	  	 	<td><div class="d-hash"><a class="line1">{{item.to}}</a></div></td>
+                	  	 	<td><div class=""><span class="text-pri-default">{{item.amount}}</span></div></td>
+                	  	 	<td><div class=""><span class="text-pri-default">{{item.fee}}</span></div></td>
+                	  	 </tr>
+                	  </table>
+                  </div> 
                     <div class="page-block">
                       
                         <el-pagination
@@ -73,12 +67,13 @@
      data(){
         return {
          tabindex:2,
+         sortType:0,//1升序，2降序
          blockList:[
+           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'31049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
+           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'32049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
-           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
-           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
-           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
+           {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'3049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0},
@@ -86,6 +81,7 @@
            {hash:'179b8ddf22d2c7f3a86b6f33ffef0cc05b6958d03299f599ffb54b9e1c6f0157',nonce:'33049',created_at:'2020-11-16T05:36:29.000+0000',from:'0xb2de23a3d3ae9fc31af7267a046f1f2bb396dc5b',to:'0x35269977f0a9f687b3368a04ae61d735a91ffd5f',amount:'20',fee:0}
            
          ],
+         defaultBlockList:[],
          totalElements:6000, //总条数
          pageSize:10,//默认每页条数
          currentPage:0,//当前页
@@ -114,9 +110,64 @@
             }
         }
      },
+     mounted(){
+     	let that = this;
+     	that.defaultBlockList = JSON.parse(JSON.stringify(that.blockList))
+     },
      methods:{
-      
-      
+      sort(num){
+      	
+      	
+      	let sortType = 0
+      	if(this.sortType==0){
+      		if(num==1){
+	      		this.blockList.sort(this.compare1('nonce'))
+	      	}else{
+	      		this.blockList.sort(this.compare('nonce'))
+	      	}
+	      	sortType = num;
+      	}
+      	
+      	if(this.sortType==1){
+      		if(num==1){
+      			sortType = 0
+      			this.blockList = JSON.parse(JSON.stringify(this.defaultBlockList))
+      			
+      		}else{
+      			sortType = num;
+      			this.blockList.sort(this.compare('nonce'))
+      		}
+      	}
+      	
+      	if(this.sortType==2){
+      		if(num==2){
+      			sortType = 0
+      			this.blockList = JSON.parse(JSON.stringify(this.defaultBlockList))
+      		}else{
+      			sortType = num;
+      			this.blockList.sort(this.compare1('nonce'))
+      		}
+      	}
+      	this.sortType = sortType
+      	//console.log(this.sortType)
+      },
+      //降序
+      compare(attr) {
+          return function(a,b){
+              var val1 = a[attr];
+              var val2 = b[attr];
+              return val2 - val1;
+          }
+          
+      },
+      //升序
+      compare1(attr) {
+          return function(a,b){
+              var val1 = a[attr];
+              var val2 = b[attr];
+              return val1 - val2;
+          }
+      },
       //表格酒奇偶行显示不一样
       tableRowClassName({row, rowIndex}) {
         if (rowIndex%2 === 1) {
@@ -128,7 +179,7 @@
       },
       //表头单元格的className的回调方法
       cellClass(row){
-        if(row.columnIndex==0){
+        if(row.columnIndex==0 || row.columnIndex==4 || row.columnIndex==5){
           return 'ntab-hash';
         }
 
